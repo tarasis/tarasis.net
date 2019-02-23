5446a707c3fdda3c58c6bcb4e69b66caebcab64c
Robert McGovern <rob@tarasis.net>
2019-02-23 01:47:05 +0100

    Added all files, basic. Still messing

---
+++ _posts/2019-02-21-first-post.md
@@ -0,0 +1,8 @@
+---
+layout: single
+title: "First Post"
+date: 2019-02-21T23:54:00-00:00
+---
+
+Just a test post. This is the first, of a new blog that is powered by **Jekyll**y
+
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": ["test","blargh"],
+        "url": "http://0.0.0.0:4000/jekyll/update/welcome-to-jekyll/",
+        "teaser":null},{
+        "title": "First Post",
+        "excerpt":"Just a test post. This is the first, of a new blog that is powered by Jekylly   ","categories": [],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/first-post/",
+        "teaser":null},]
