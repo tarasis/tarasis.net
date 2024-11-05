5278e59061b7f5d9e9bc4ea2dcd68a4315b6f58c
Robert McGovern <rob@tarasis.net>
2024-11-05 22:47:44 +0000

    minor update to check preview is working

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tarasis.net V5 by Robert McGovern'
-export const SITE_DESCRIPTION = 'Personal website & blog of Robert McGovern'
+export const SITE_TITLE = 'Tarasis.net V5 WIP by Robert McGovern'
+export const SITE_DESCRIPTION =
+	'WIP rebuild of the personal website & blog of Robert McGovern, moving from Eleventy/11ty to Astro'
