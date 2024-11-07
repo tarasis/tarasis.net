d690ef4895cb2900c5dd9fac24415b721d55ca71
Robert McGovern <rob@tarasis.net>
2024-11-07 13:06:21 +0000

    Added Expressive-Code & Color Chips
    Added to plugins for code highlighting. First is Expressive Code which
    is what is used in the Astro docs which allows a lot more control. The
    other is a plugin to EC which adds colour chips beside CSS declared
    colours.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import expressiveCode from 'astro-expressive-code';
+import { pluginColorChips } from 'expressive-code-color-chips';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://v5.tarasis.net',
+    integrations: [expressiveCode({
+		plugins: [pluginColorChips()],
+	}), mdx(), sitemap()],
 });
