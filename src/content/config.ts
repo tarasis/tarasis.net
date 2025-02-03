47067c9b394f4921f5ae98f6be97a0f3d9ce7e78
Robert McGovern <rob@tarasis.net>
2025-02-03 01:40:56 +0000

    wip redoing css, testing to different theme selectors

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,11 +6,21 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional()
+		heroImage: z.string().optional(),
+		draft: z.boolean().optional().default(false),
+		description: z.string().optional().default(''),
+		image: z.string().optional().default(''),
+		tags: z.array(z.string()).optional().default([]),
+		category: z.string().optional().default(''),
+		lang: z.string().optional().default(''),
+		/* For internal use */
+		prevTitle: z.string().default(''),
+		prevSlug: z.string().default(''),
+		nextTitle: z.string().default(''),
+		nextSlug: z.string().default('')
 	})
 })
 
